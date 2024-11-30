f3192f61ebfd0b74e88bdae11932ff1b13bf92db
Cyforce <celian.touzeau@outlook.com>
2024-11-30 09:42:40 +0100

    changes

--- site/.next/types/app/layout.ts
+++ site/.next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\Utilisateur\Documents\Projets\Portfolio\portfolio\app\layout.tsx
+// File: C:\Users\Utilisateur\Documents\Projets\Portfolio\portfolio\site\app\layout.tsx
 import * as entry from '../../../app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
--- site/next.config.ts
+++ site/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  // Cette configuration empêche Next.js de traiter les fichiers statiques comme des pages
+  async redirects() {
+    return [];
+  },
+  async rewrites() {
+    return [];
+  },
 };
 
 export default nextConfig;
